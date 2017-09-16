e89f45b91024b2d500ce3b9b25828a91518675bb
Peter Liu <Peterljr888@gmail.com>
2017-09-16 08:52:02 -0400

    run scripts through shellcheck, apply fixes

--- recording/check-disk-space.sh
+++ recording/check-disk-space.sh
@@ -1,4 +1,4 @@
-#!/bin/sh  
+#!/bin/bash
 #
 # check-disk-space.sh
 #
@@ -6,13 +6,11 @@
 #
 #   Note: Gmail SMTP limited to 500 emails per day.
 #
-#   Default crontab schedule: "0 0 * * * " or "@daily"
+#   Default crontab schedule:
+#       "@daily" or "0 0 * * * "
 #
 #####################################################################################################################################
 
-YR=$(date +%Y)                                                           ## set variable $YR for year (4 digits)
-MO=$(date +%m)                                                           ## set variable $MO for month
-DAY=$(date +%d)                                                          ## set variable $DAY for day
 NOW=$(date +'%Y/%m/%d %T')                                               ## sets date / time variable $NOW
 
 EXT_FS="/dev/sdc2"                  ## Filesystem name for external hard drive
@@ -28,42 +26,46 @@ REM_7D=$REM_7D_192
 
 ## Check external drive
 if [ -d ${EXT_FS} ]; then
-    EXT_FREE=`df -k --output=avail $EXT_FS | tail -n1`
+    EXT_FREE=$( df -k --output=avail $EXT_FS | tail -n1 )
+    EXT_CHK_7D=$( echo "$EXT_FREE < $REM_7D" | bc )
+    EXT_CHK_30D=$( echo "$EXT_FREE < $REM_30D" | bc )
 
     ## Compare current free space amount in external drive to calculated remaining disk space for 7 days of 192kbps recordings
-    if (( echo "$EXT_FREE < $REM_7D" | bc )); then
+    if [ "$EXT_CHK_7D" -eq 1 ]; then
 
         ## Log message
-        echo $NOW "[DISK CHECK]: Less than 7 days free space remaining in $EXT_FS: $EXT_FREE KB remaining"
+        echo "$NOW [DISK CHECK]: Less than 7 days free space remaining in $EXT_FS: $EXT_FREE KB remaining"
 
         ## Email alert notification
-        echo $NOW "Less than 7 days free space of 192kbps recordings remaining in $EXT_FS: $EXT_FREE KB remaining" | mail -a "From: Recordings Server <recordings@wkdu.org>" -s "[WARNING] Less than 7 days free space available in EXTERNAL drive" admin@wkdu.org
+        echo "$NOW Less than 7 days free space of 192kbps recordings remaining in $EXT_FS: $EXT_FREE KB remaining" | mail -a "From: Recordings Server <recordings@wkdu.org>" -s "[WARNING] Less than 7 days free space available in EXTERNAL drive" admin@wkdu.org
 
-    else if (( echo "$EXT_FREE < $REM_30D" | bc )); then
+    elif [ "$EXT_CHK_30D" -eq 1 ]; then
 
-        echo $NOW "[DISK CHECK]: Less than 30 days free space remaining in $EXT_FS: $EXT_FREE KB remaining"
-        echo $NOW "Less than 30 days free space of 320kbps recordings remaining in $EXT_FS: $EXT_FREE KB remaining" | mail -a "From: Recordings Server <recordings@wkdu.org>" -s "[WARNING] Less than 30 days free space available in EXTERNAL drive" admin@wkdu.org
+        echo "$NOW [DISK CHECK]: Less than 30 days free space remaining in $EXT_FS: $EXT_FREE KB remaining"
+        echo "$NOW Less than 30 days free space of 320kbps recordings remaining in $EXT_FS: $EXT_FREE KB remaining" | mail -a "From: Recordings Server <recordings@wkdu.org>" -s "[WARNING] Less than 30 days free space available in EXTERNAL drive" admin@wkdu.org
 
     fi
 fi
 
 ## Check /data/ partition
 if [ -d /data/ ]; then
-    DATA_FREE=`df -k --output=avail /data/ | tail -n1`
+    DATA_FREE=$( df -k --output=avail /data/ | tail -n1 )
+    DATA_CHK_7D=$( echo "$DATA_FREE < $REM_7D" | bc )
+    DATA_CHK_30D=$( echo "$DATA_FREE < $REM_30D" | bc )
 
     ## Compare current free space amount in internal drive partition to calculated remaining disk space for 7 days of 192kbps recordings
-    if (( echo "$DATA_FREE < $REM_7D" | bc )); then
+    if [ "$DATA_CHK_7D" -eq 1 ]; then
 
         ## Log message
-        echo $NOW "[DISK CHECK]: Less than 7 days free space remaining in /data/: $DATA_FREE KB remaining"
+        echo "$NOW [DISK CHECK]: Less than 7 days free space remaining in /data/: $DATA_FREE KB remaining"
 
         ## Email alert notification
-        echo $NOW "Less than 7 days free space of 192kbps recordings remaining in /data/: $DATA_FREE KB remaining" | mail -a "From: Recordings Server <recordings@wkdu.org>" -s "[WARNING] Less than 7 days free space available in INTERNAL drive partition /data/" admin@wkdu.org
+        echo "$NOW Less than 7 days free space of 192kbps recordings remaining in /data/: $DATA_FREE KB remaining" | mail -a "From: Recordings Server <recordings@wkdu.org>" -s "[WARNING] Less than 7 days free space available in INTERNAL drive partition /data/" admin@wkdu.org
 
-    else if (( echo "$DATA_FREE < $REM_30D" | bc )); then
+    elif [ "$DATA_CHK_30D" -eq 1 ]; then
 
-        echo $NOW "[DISK CHECK]: Less than 30 days free space remaining in /data/: $DATA_FREE KB remaining"
-        echo $NOW "Less than 30 days free space of 320kbps recordings remaining in /data/: $DATA_FREE KB remaining" | mail -a "From: Recordings Server <recordings@wkdu.org>" -s "[WARNING] Less than 30 days free space available in INTERNAL drive partition /data/" admin@wkdu.org
+        echo "$NOW [DISK CHECK]: Less than 30 days free space remaining in /data/: $DATA_FREE KB remaining"
+        echo "$NOW Less than 30 days free space of 320kbps recordings remaining in /data/: $DATA_FREE KB remaining" | mail -a "From: Recordings Server <recordings@wkdu.org>" -s "[WARNING] Less than 30 days free space available in INTERNAL drive partition /data/" admin@wkdu.org
 
     fi
 fi
--- recording/create-recordings.sh
+++ recording/create-recordings.sh
@@ -1,4 +1,4 @@
-#!/bin/sh  
+#!/bin/bash
 #
 # create-recordings.sh
 #
@@ -12,60 +12,76 @@
 #
 #   Note: Gmail SMTP limited to 500 emails per day.
 #
-#   Default crontab schedule: "0 * * * *" or "@hourly"
+#   Default crontab schedule:
+#       "@hourly" or "0 * * * *"
 #
 #####################################################################################################################################
 
 ##  First we determine today's date and set variables $YR, $MO, and $DAY.
 ##  Also set variable $NOW for current date and time.
-YR=$(date +%Y)                                                           ## set variable $YR for year (4 digits)
-MO=$(date +%m)                                                           ## set variable $MO for month
-DAY=$(date +%d)                                                          ## set variable $DAY for day
-NOW=$(date +'%Y/%m/%d %T')                                               ## sets date / time variable $NOW
+YR=$(date +%Y)                                                          ## set variable $YR for year (4 digits)
+MO=$(date +%m)                                                          ## set variable $MO for month
+DAY=$(date +%d)                                                         ## set variable $DAY for day
+NOW=$(date +'%Y/%m/%d %T')                                              ## sets date / time variable $NOW
 
 ##  Next we determine the current time and set variables $H and $M.
-H=$(date +%H)                                                            ## set variable $H for hour (24 hour)
-M=$(date +%M)                                                            ## set variable $M for minute
-echo $NOW "Starting recording to audio file "$YR-$MO-$DAY-$H$M".mp3"     ## puts start time entry in record.log
+H=$(date +%H)                                                           ## set variable $H for hour (24 hour)
+M=$(date +%M)                                                           ## set variable $M for minute
+echo "$NOW Starting recording to audio file $YR-$MO-$DAY-$H$M.mp3"      ## puts start time entry in record.log
 
 ## Default external drive location (may need to be updated if the drive name changes)
 EXT_DIR="/media/peter/Recordings"
 
 ## Create directory for today's recordings
 if [ -d ${EXT_DIR} ]; then                                      ## Test to see if external drive directory exists
-    if [ ! -d "$EXT_DIR/wkdu/$YR-$MO-$DAY" ]; then              ## Check for existing directory for today, if not make one
-        echo $NOW "Creating directory in external drive: $EXT_DIR/wkdu/$YR-$MO-$DAY"
-        mkdir ${EXT_DIR}/wkdu/$YR-$MO-$DAY
+
+    ## Check for existing directory for today, if not make one
+    if [ ! -d "$EXT_DIR/wkdu/$YR-$MO-$DAY" ]; then              
+        echo "$NOW Creating directory in external drive: $EXT_DIR/wkdu/$YR-$MO-$DAY"
+        mkdir ${EXT_DIR}"/wkdu/$YR-$MO-$DAY"
     fi
+
 else
-    EXT2=`df -h | grep '/dev/sdc2' | awk '{print $6}'`          ## Check for a different external drive location since it was unable to find the default
-    if [ -d ${EXT2} ]; then
+
+    EXT2=$(df -h | grep '/dev/sdc2' | awk '{print $6}')         
+
+     ## Check for a different external drive location since it was unable to find the default
+    if [ -d "$EXT2" ]; then
         EXT_DIR=EXT2
-        if [ ! -d "$EXT2/wkdu/$YR-$MO-$DAY"]; then
-            echo $NOW "Creating directory in external drive: $EXT2/wkdu/$YR-$MO-$DAY"
-            mkdir ${EXT2}/wkdu/$YR-$MO-$DAY
+        if [ ! -d "$EXT2/wkdu/$YR-$MO-$DAY" ]; then
+            echo "$NOW Creating directory in external drive: $EXT2/wkdu/$YR-$MO-$DAY"
+            mkdir "$EXT2/wkdu/$YR-$MO-$DAY"
         fi
-    else if [ ! -d "/data/recordings/$YR-$MO-$DAY/"]; then      ## Still couldn't find external drive, let's check for a folder on the computer's internal drives
-        echo $NOW "Creating directory in internal drive: /data/recordings/$YR-$MO-$DAY/"
-        mkdir /data/recordings/$YR-$MO-$DAY
+
+    ## Still couldn't find external drive, let's check for a folder on the computer's internal drives
+    elif [ ! -d "/data/recordings/$YR-$MO-$DAY/" ]; then      
+        echo "$NOW Creating directory in internal drive: /data/recordings/$YR-$MO-$DAY/"
+        mkdir "/data/recordings/$YR-$MO-$DAY"
     fi
+
 fi
 
-echo $NOW "Audio file: $YR-$MO-$DAY-$H$M.mp3"                   ## puts start time entry in record.log
+echo "$NOW Audio file: $YR-$MO-$DAY-$H$M.mp3"                           ## puts start time entry in record.log
 
 ## Start a 1 hour recording at CD quality (16bit little endian, stereo, 44.1KHz) and pipe to compressed MP3 format
 if [ -d ${EXT_DIR} ]; then
-    echo $NOW "Starting recording audio file to external drive: $EXT_DIR"
-    arecord -D "plughw:1,0" -d3600 -f cd -t raw | lame -r -b 192 - ${EXT_DIR}/wkdu/$YR-$MO-$DAY/$YR-$MO-$DAY-$H$M.mp3             # Records audio stream for 60 minutes (3600 seconds)
-else
-    ## Send out email alert
-    echo $NOW "Unable to find external drive during cronjob. Started recording in internal drive instead: /data/recordings/$YR-$MO-$DAY/$YR-$MO-$DAY-$H$M.mp3" | mail -a "From: Recordings Server <recordings@wkdu.org>" -s "[ALERT] Created new recording in INTERNAL drive" admin@wkdu.org
 
-    echo $NOW "Starting recording audio file to internal drive: /data/recordings (could not find external drive)"
-    arecord -D "plughw:1,0" -d3600 -f cd -t raw | lame -r -b 192 - /data/recordings/$YR-$MO-$DAY/$YR-$MO-$DAY-$H$M.mp3              # Records audio stream for 60 minutes (3600 seconds)
+    echo "$NOW Starting recording audio file to external drive: $EXT_DIR"
+
+    ## Records audio stream for 60 minutes (3600 seconds)
+    arecord -D "plughw:1,0" -d3600 -f cd -t raw -q | lame -r -b 192 - "$EXT_DIR/wkdu/$YR-$MO-$DAY/$YR-$MO-$DAY-$H$M.mp3" -quiet
+
+else
+
+    ## Send out email alert
+    echo "$NOW Unable to find external drive during cronjob. Started recording in internal drive instead: /data/recordings/$YR-$MO-$DAY/$YR-$MO-$DAY-$H$M.mp3" | mail -a "From: Recordings Server <recordings@wkdu.org>" -s "[ALERT] Created new recording in INTERNAL drive" admin@wkdu.org
+    echo "$NOW Starting recording audio file to internal drive: /data/recordings (could not find external drive)"
+
+    ## Records audio stream for 60 minutes (3600 seconds)
+    arecord -D "plughw:1,0" -d3600 -f cd -t raw -q | lame -r -b 192 - "/data/recordings/$YR-$MO-$DAY/$YR-$MO-$DAY-$H$M.mp3" -quiet
 fi
 
-NOW=$(date +'%Y/%m/%d %T')                                               ## resets date / time variable $NOW to current time
-echo $NOW "Finished recording audio file: $YR-$MO-$DAY-$H$M.mp3"               ## puts stop recording time entry in record.log
-echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"  ## puts separation line in record.log
-                
+NOW=$(date +'%Y/%m/%d %T')                                              ## resets date / time variable $NOW to current time
+echo "$NOW Finished recording audio file: $YR-$MO-$DAY-$H$M.mp3"        ## puts stop recording time entry in record.log
+echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"  ## puts separation line in record.log
+

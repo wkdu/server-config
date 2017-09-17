27e3ca001e5a4277b8fcf744f0ccd35d9b5dfab7
Peter Liu <Peterljr888@gmail.com>
2017-09-17 15:18:23 -0400

    add contine-recording script, update readme

--- recording/README.md
+++ recording/README.md
@@ -6,10 +6,10 @@ On the server, the files are located at `/media/peter/Recordings/wkdu` mounted o
 
 | File name | Location on server | Purpose | Crontab scheduling |
 | --------- | ------------------ | ------- | ------------------ |
-| create-directory.sh | /home/peter/bin/ | Create directory for recordings each day on the external drive (or internal drive if unable to find external drive) | "@daily" |
-| create-recording.sh | /home/peter/bin/ | Create recordings on the external drive (or internal drive if unable to find external drive) by recording audio from the AudioBox USB | "@hourly" |
-| continue-recording.sh | /home/peter/bin/ | Similar to `create-recording.sh` except it calculates how long until the end of the hour and creates a recording of that duration (in case server unexpectedly restarts) | "@reboot" |
-| check-disk-space.sh | /home/peter/bin/ | Check disk space where recordings are stored and send an email notification if there is low disk space (at 7 and 30 days of recordings left) | "@daily" |
+| create-directory.sh | /home/peter/bin/ | Create directory for recordings each day on the external drive (or internal drive if unable to find external drive) | "0 0 * * *" (daily) |
+| create-recording.sh | /home/peter/bin/ | Create recordings on the external drive (or internal drive if unable to find external drive) by recording audio from the AudioBox USB | "0 * * * *" (hourly) |
+| continue-recording.sh | /home/peter/bin/ | Similar to `create-recording.sh` except it calculates how long until the end of the hour and creates a recording of that duration (in case server unexpectedly restarts) | (on reboot) |
+| check-disk-space.sh | /home/peter/bin/ | Check disk space where recordings are stored and send an email notification if there is low disk space (at 7 and 30 days of recordings left) | "1 0 * * *" (daily) |
 | .asoundrc | /home/peter/ | ALSA sound config (sets up Audiobox USB audio device to record, uses `plug` chained with `dsnoop` to be able to record from the same device simultaneously) | n/a |
 | recordings.wkdu.org | /etc/nginx/sites-available/ | Nginx configuration for recordings.wkdu.org URL | n/a |
 
---
+++ recording/continue-recording.sh
@@ -0,0 +1,60 @@
+#!/bin/bash  
+#
+# continue-recording.sh
+#
+# This is a script that will continue recording an audio stream (in case of computer reboot)
+#
+#   arecord program significant options:
+#
+#       -r, --rate=#<Hz> Sampling rate in Hertz.
+#       -d, --duration=# Interrupt recording after # seconds.
+#       -c, --channels=# The number of channels.  The default is one channel.
+#
+#   Note: Gmail SMTP limited to 500 emails per day.
+#
+#   Default crontab schedule: 
+#       "@reboot"
+#
+#####################################################################################################################################
+
+##  First we determine today's date and set variables $YR, $MO, and $DAY.
+##  Also set variable $NOW for current date and time.
+YR=$(date +%Y)                                                                                  ## set variable $YR for year (4 digits)
+MO=$(date +%m)                                                                                  ## set variable $MO for month
+DAY=$(date +%d)                                                                                 ## set variable $DAY for day
+NOW=$(date +'%Y/%m/%d %T')                                                                      ## sets date / time variable $NOW
+
+##  Next we determine the current time and set variables $H and $M.
+H=$(date +%H)                                                                                   ## set variable $H for hour (24 hour)
+M=$(date +%M)                                                                                   ## set variable $M for minute
+S=$(date +%S)
+
+echo "$NOW Continuing $YR-$MO-$DAY-$H""00 recording with audio file $YR-$MO-$DAY-$H$M.mp3"      ## puts start time entry in record.log
+
+## Calculate time left in current hour to get remaining duration
+TIME_REM=$(echo "3600 - (($M * 60) + $S)" | bc )
+
+## Default external drive location (may need to be updated if the drive name changes)
+EXT_DIR="/media/peter/Recordings"
+
+## Start a 1 hour recording at CD quality (16bit little endian, stereo, 44.1KHz) and pipe to compressed MP3 format
+if [ -d ${EXT_DIR} ]; then
+
+    echo "$NOW Started recording audio file to external drive: $EXT_DIR/wkdu/$YR-$MO-$DAY/$YR-$MO-$DAY-$H$M.mp3"
+
+    ## Records audio stream for 60 minutes (3600 seconds)
+    arecord -D plug:snoop -d "$TIME_REM" -f cd -t raw -q | lame -r -b 192 - "$EXT_DIR/wkdu/$YR-$MO-$DAY/$YR-$MO-$DAY-$H$M.mp3" -quiet
+
+else
+
+    ## Send out email alert
+    echo "$NOW Unable to find external drive during continue-recording cronjob. Started recording in internal drive instead: /data/recordings/$YR-$MO-$DAY/$YR-$MO-$DAY-$H$M.mp3" | mail -a "From: Recordings Server <recordings@wkdu.org>" -s "[ALERT] Created new recording in INTERNAL drive" admin@wkdu.org
+    echo "$NOW Started recording audio file to internal drive: /data/recordings (could not find external drive)"
+
+    ## Records audio stream for 60 minutes (3600 seconds)
+    arecord -D plug:snoop -d "$TIME_REM" -f cd -t raw -q | lame -r -b 192 - "/data/recordings/$YR-$MO-$DAY/$YR-$MO-$DAY-$H$M.mp3" -quiet
+
+fi
+
+NOW=$(date +'%Y/%m/%d %T')                                              ## resets date / time variable $NOW to current time
+echo "$NOW Finished recording audio file: $YR-$MO-$DAY-$H$M.mp3"        ## puts stop recording time entry in record.log

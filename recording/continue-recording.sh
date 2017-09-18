b24b909b598107d82faa922ed201e9de72169a97
Peter Liu <me@peterliu.io>
2017-09-17 22:53:03 -0400

    proper flags for lame encoder

--- recording/continue-recording.sh
+++ recording/continue-recording.sh
@@ -43,7 +43,7 @@ if [ -d ${EXT_DIR} ]; then
     echo "$NOW Started recording audio file to external drive: $EXT_DIR/wkdu/$YR-$MO-$DAY/$YR-$MO-$DAY-$H$M.mp3"
 
     ## Records audio stream for 60 minutes (3600 seconds)
-    arecord -D plug:snoop -d "$TIME_REM" -f cd -t raw -q | lame -r -b 192 - "$EXT_DIR/wkdu/$YR-$MO-$DAY/$YR-$MO-$DAY-$H$M.mp3" -quiet
+    arecord -D plug:snoop -d "$TIME_REM" -f cd -t raw -q | lame -r -b 192 - "$EXT_DIR/wkdu/$YR-$MO-$DAY/$YR-$MO-$DAY-$H$M.mp3" --quiet
 
 else
 
@@ -52,7 +52,7 @@ else
     echo "$NOW Started recording audio file to internal drive: /data/recordings (could not find external drive)"
 
     ## Records audio stream for 60 minutes (3600 seconds)
-    arecord -D plug:snoop -d "$TIME_REM" -f cd -t raw -q | lame -r -b 192 - "/data/recordings/$YR-$MO-$DAY/$YR-$MO-$DAY-$H$M.mp3" -quiet
+    arecord -D plug:snoop -d "$TIME_REM" -f cd -t raw -q | lame -r -b 192 - "/data/recordings/$YR-$MO-$DAY/$YR-$MO-$DAY-$H$M.mp3" --quiet
 
 fi
 
